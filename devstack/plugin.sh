238d912548ddb8e159610f12bbb81348d87c7284
Huan Xie <huan.xie@citrix.com>
2016-11-16 00:25:14 -0800

    WIP: Add devstack-plugins in os-xenapi
    When changed to use os-xenapi, we also need to change the way
    we install OpenStack by DevStack. This patch use the plugins
    mechanisem DevStack provided, we can install the extra/specific
    parts besides what DevStack already done.
    1. Install Dom0 plugins
    2. Install session part as real codes not on pypi (maybe remove
       it in the future)
    3. Add some todo, which can configure XenServer+Neutron specific
       part here
    
    Change-Id: I6dc783eeb5c95e5fe81d907154aa2e880d6bac9e

---
+++ devstack/plugin.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Copyright 2013 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+MODE=$1
+PHASE=$2
+
+function get_dom0_ssh {
+    local dom0_ip
+    dom0_ip=$(echo "$XENAPI_CONNECTION_URL" | cut -d "/" -f 3)
+
+    local ssh_dom0
+    ssh_dom0="sudo -u $DOMZERO_USER ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$dom0_ip"
+    echo $ssh_dom0
+    return 0
+}
+
+# Install
+function install_plugins {
+    local ssh_dom0
+    ssh_dom0=$(get_dom0_ssh)
+
+    local dom0_func
+    dom0_func=`cat $TOP_DIR/tools/xen/functions`
+    local dom0_plugin_dir
+    dom0_plugin_dir=`$ssh_dom0 "$dom0_func; set -eux; dom0_plugin_location"`
+
+    # Get the path that os-xenapi is installed
+    local plugin_dir
+    plugin_dir=$(pip show os-xenapi |grep "Location:"|cut -d " " -f 2-)
+    tar -czf - -C $plugin_dir/os_xenapi/dom0/etc/xapi.d/plugins/ ./ |
+        $ssh_dom0 "tar -xzf - -C $dom0_plugin_dir && chmod a+x $dom0_plugin_dir/*"
+}
+
+if [[ "$MODE" == "stack" ]]; then
+    case "$PHASE" in
+        install)
+            install_plugins
+            ;;
+        post-config)
+            # Called after the layer 1 and 2 services have been configured.
+            # All configuration files for enabled services should exist at this point.
+            # TODO(huanxie) when reverse q-agt/q-domua merged, q-domua is XS specific part
+            # Configure XenServer neutron specific items for q-domua
+            # ovs native mode
+            # ovs VxLAN
+            ;;
+        extra)
+            ;;
+        test-config)
+            # Called at the end of devstack used to configure tempest
+            # or any other test environments
+            # TODO(huanxie) Maybe we can set some conf here for CI?
+            ;;
+    esac
+elif [[ "$MODE" == "clean" ]]; then
+    # Called by clean.sh before other services are cleaned, but after unstack.sh has been called
+    # TODO(huanxie)
+    # Stop q-domua in the future?
+    # clean the OVS bridge created in Dom0 and iptables rules?
+    echo "mode is clean"
+fi
