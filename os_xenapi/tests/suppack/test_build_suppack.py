7c4372ad88c390edd4c386aa0b0a5aa2c3c7d93e
Huan Xie <huan.xie@citrix.com>
2016-11-21 01:35:26 -0800

    Move scripts of building rpm/iso to os-xenapi
    This patch is to move those scripts that build OpenStack dom0
    plugins into os-xenapi repo.
    1. scripts to build RPMs
    2. scripts to build supplemental iso
    3. unit tests to triiger build iso (including build rpm)
    
    Change-Id: Ibdc17c436e794b474c72f6a5f4686b3a77c58aa2

---
+++ os_xenapi/tests/suppack/test_build_suppack.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from os_xenapi.tests import base
+
+
+class PluginTestBase(base.TestCase):
+    def test_build_suppack(self):
+        test_path = os.path.dirname(__file__)
+        print('test path: %s' % test_path)
+        relative_path = "../../dom0"
+        for item in relative_path.split('/'):
+            test_path = os.path.join(test_path, item)
+        dom0_path = os.path.realpath(test_path)
+        print('dom0 path: %s' % dom0_path)
+        os.system('%s/suppack/build-xenserver-suppack.sh' % dom0_path)
+        self.assertTrue(
+            os.path.exists('%s/build/conntrack-tools.iso' % dom0_path))
+        self.assertTrue(
+            os.path.exists('%s/build/conntrack-tools.iso.md5' % dom0_path))
+        self.assertTrue(
+            os.path.exists('%s/build/conntrack-tools.metadata.md5'
+                           % dom0_path))
+        self.assertTrue(
+            os.path.exists('%s/build/xenapi-plugins-upstream.iso' % dom0_path))
+        self.assertTrue(
+            os.path.exists('%s/build/xenapi-plugins-upstream.iso.md5'
+                           % dom0_path))
+        self.assertTrue(
+            os.path.exists('%s/build/xenapi-plugins-upstream.metadata.md5'
+                           % dom0_path))
