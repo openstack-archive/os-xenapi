7796cf0ac9c80b7deb8990ffdd1ce99ac5c23900
Huan Xie <huan.xie@citrix.com>
2017-02-16 20:06:44 -0800

    Enable neutron VxLAN
    1. Set ovs-agent (q-domua) configurations in ml2_conf.ini.domU
    including enable iptables in dom0 for ovs native mode
    2. Enable VxLAN by configuring the tunnel between dom0 and domU
    and enable port 4789 in dom0
    3. Install neutron dom0 plugin
    4. Configure nova.conf to use the XenAPIOpenVswitchDriver
    
    Note:
    I also made a patch I8a31c81d9475387fe4ed7030b70b26098e588771
    in devstack, which removed the duplicated code added in this
    patch, we can also monitor that patch's CI test result
    
    Change-Id: Ic816404c84f6a8899d01a77cb67fbfb421653e6b

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -20,6 +20,10 @@ PHASE=$2
 
 OS_XENAPI_DIR=$DEST/os-xenapi
 
+OVS_PORT="6640"
+VXLAN_PORT="4789"
+XS_DOM0_NEUTRON="XenServerDevstack"
+
 function get_dom0_ssh {
     local dom0_ip
     dom0_ip=$(echo "$XENAPI_CONNECTION_URL" | cut -d "/" -f 3)
@@ -30,7 +34,7 @@ function get_dom0_ssh {
     return 0
 }
 
-# Install Dom0 plugins
+# Install Nova and Neutron Dom0 plugins
 function install_plugins {
     local ssh_dom0
     ssh_dom0=$(get_dom0_ssh)
@@ -55,20 +59,130 @@ function install_plugins {
         tar -czf - -C $plugin_dir/os_xenapi/dom0/etc/xapi.d/plugins/ ./ |
             $ssh_dom0 "tar -xzf - -C $dom0_plugin_dir && chmod a+x $dom0_plugin_dir/*"
     fi
+
+    # install neutron dom0 plugins to dom0 as it's not moved to os-xenapi
+    tar -czf - -C $NEUTRON_DIR/neutron/plugins/ml2/drivers/openvswitch/agent/xenapi/etc/xapi.d/plugins/ ./ |
+        $ssh_dom0 "tar -xzf - -C $dom0_plugin_dir && chmod a+x $dom0_plugin_dir/*"
+}
+
+# Config iptables in Dom0
+function config_iptables {
+    local ssh_dom0
+    ssh_dom0=$(get_dom0_ssh)
+
+    # Remove restriction on linux bridge in Dom0 since neutron is default
+    $ssh_dom0 "rm -f /etc/modprobe.d/blacklist-bridge*"
+
+    # Save errexit setting
+    _ERREXIT_XENSERVER=$(set +o | grep errexit)
+    set +o errexit
+
+    # Check Dom0 internal chain for Neutron, add if not exist
+    $ssh_dom0 "iptables -t filter -L $XS_DOM0_NEUTRON"
+    local chain_result=$?
+    if [ "$chain_result" != "0" ]; then
+        $ssh_dom0 "iptables -t filter --new $XS_DOM0_NEUTRON"
+        $ssh_dom0 "iptables -t filter -I INPUT -j $XS_DOM0_NEUTRON"
+    fi
+
+    # Check iptables for remote ovsdb connection, add if not exist
+    $ssh_dom0 "iptables -t filter -C $XS_DOM0_NEUTRON -p tcp -m tcp --dport $OVS_PORT -j ACCEPT"
+    local remote_conn_result=$?
+    if [ "$remote_conn_result" != "0" ]; then
+        $ssh_dom0 "iptables -t filter -I $XS_DOM0_NEUTRON -p tcp --dport $OVS_PORT -j ACCEPT"
+    fi
+
+    # Check iptables for VxLAN, add if not exist
+    $ssh_dom0 "iptables -t filter -C $XS_DOM0_NEUTRON -p udp -m multiport --dports $VXLAN_PORT -j ACCEPT"
+    local vxlan_result=$?
+    if [ "$vxlan_result" != "0" ]; then
+        $ssh_dom0 "iptables -t filter -I $XS_DOM0_NEUTRON -p udp -m multiport --dport $VXLAN_PORT -j ACCEPT"
+    fi
+
+    # Restore errexit setting
+    $_ERREXIT_XENSERVER
+}
+
+# Configure ovs agent for compute node, i.e. q-domua
+function config_ovs_agent {
+    # TODO(huan): remove below line when devstack is cleanup with dom0
+    sudo rm -f "/$Q_PLUGIN_CONF_FILE.domU"
+
+    # Make a copy of our config for domU
+    sudo cp /$Q_PLUGIN_CONF_FILE "/$Q_PLUGIN_CONF_FILE.domU"
+
+    # Change domU's config file to STACK_USER
+    sudo chown $STACK_USER:$STACK_USER /$Q_PLUGIN_CONF_FILE.domU
+
+    # Configure xen configuration for neutron rootwrap.conf
+    iniset $Q_RR_CONF_FILE xenapi xenapi_connection_url "$XENAPI_CONNECTION_URL"
+    iniset $Q_RR_CONF_FILE xenapi xenapi_connection_username "$XENAPI_USER"
+    iniset $Q_RR_CONF_FILE xenapi xenapi_connection_password "$XENAPI_PASSWORD"
+
+    # Under XS/XCP, the ovs agent needs to target the dom0
+    # integration bridge.  This is enabled by using a root wrapper
+    # that executes commands on dom0 via a XenAPI plugin.
+    # XenAPI does not support daemon rootwrap now, so set root_helper_daemon empty
+    # Deal with Dom0's L2 Agent:
+    Q_RR_DOM0_COMMAND="$NEUTRON_BIN_DIR/neutron-rootwrap-xen-dom0 $Q_RR_CONF_FILE"
+    iniset "/$Q_PLUGIN_CONF_FILE.domU" agent root_helper "$Q_RR_DOM0_COMMAND"
+    iniset "/$Q_PLUGIN_CONF_FILE.domU" agent root_helper_daemon ""
+
+    # Disable minimize polling, so that it can always detect OVS and Port changes
+    # This is a problem of xenserver + neutron, bug has been reported
+    # https://bugs.launchpad.net/neutron/+bug/1495423
+    iniset "/$Q_PLUGIN_CONF_FILE.domU" agent minimize_polling False
+
+    # XEN_INTEGRATION_BRIDGE is the integration bridge in dom0
+    iniset "/$Q_PLUGIN_CONF_FILE.domU" ovs integration_bridge $XEN_INTEGRATION_BRIDGE
+
+    # Set OVS native interface for ovs-agent in compute node
+    XEN_DOM0_IP=$(echo "$XENAPI_CONNECTION_URL" | cut -d "/" -f 3)
+    iniset /$Q_PLUGIN_CONF_FILE.domU ovs ovsdb_connection tcp:$XEN_DOM0_IP:$OVS_PORT
+    iniset /$Q_PLUGIN_CONF_FILE.domU ovs of_listen_address $HOST_IP
+
+    # Clean bridge mapping as it's copied from $Q_PLUGIN_CONF_FILE (q-agt)
+    iniset /$Q_PLUGIN_CONF_FILE.domU ovs bridge_mappings ""
+
+    if [[ "$ENABLE_TENANT_VLANS" == "True" ]]; then
+        # Create a bridge "br-$VLAN_INTERFACE" and add port
+        _neutron_ovs_base_add_bridge "br-$VLAN_INTERFACE"
+        sudo ovs-vsctl -- --may-exist add-port "br-$VLAN_INTERFACE" $VLAN_INTERFACE
+        _move_neutron_addresses_route "br-$VLAN_INTERFACE" "$VLAN_INTERFACE" False True "inet"
+
+        # Set bridge mapping for q-domua which is for compute node
+        iniset /$Q_PLUGIN_CONF_FILE.domU ovs bridge_mappings "physnet1:$FLAT_NETWORK_BRIDGE"
+
+        # Set bridge mappings for q-agt as we have an extra bridge mapping physnet1 for domU and dom0
+        iniset /$Q_PLUGIN_CONF_FILE ovs bridge_mappings "physnet1:br-$VLAN_INTERFACE,$PHYSICAL_NETWORK:$PUBLIC_BRIDGE"
+    fi
+
+    if [[ "$OVS_ENABLE_TUNNELING" == "True" ]]; then
+        # Set tunnel ip for openvswitch agent in compute node (q-domua).
+        # All q-domua's OVS commands are executed in Dom0 as Xen has the privileged and unprivileged domain,
+        # the tunnel is established between Dom0 and DomU(where devstack runs),
+        # thus we need to change the local_ip in q-domua that is used for Dom0
+        iniset /$Q_PLUGIN_CONF_FILE.domU ovs local_ip $XEN_DOM0_IP
+    fi
+}
+
+function config_nova_compute {
+    iniset $NOVA_CONF xenserver vif_driver nova.virt.xenapi.vif.XenAPIOpenVswitchDriver
+    iniset $NOVA_CONF xenserver ovs_integration_bridge $XEN_INTEGRATION_BRIDGE
+    iniset $NOVA_CONF DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 }
 
 if [[ "$MODE" == "stack" ]]; then
     case "$PHASE" in
         install)
             install_plugins
+            config_iptables
             ;;
         post-config)
             # Called after the layer 1 and 2 services have been configured.
             # All configuration files for enabled services should exist at this point.
-            # TODO(huanxie): when reverse q-agt/q-domua merged, q-domua is XS specific part
             # Configure XenServer neutron specific items for q-domua
-            # ovs native mode
-            # ovs VxLAN
+            config_ovs_agent
             ;;
         extra)
             ;;
