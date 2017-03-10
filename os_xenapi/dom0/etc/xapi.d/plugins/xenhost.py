f55f325aadc4cf77a838eff3ef9dc4f6951ca74a
Huan Xie <huan.xie@citrix.com>
2017-03-10 02:01:15 -0800

    Create ovs port with other params together
    When create ovs port in nova, it will first create ovs port using
    one commnd and set other parameters using seperated commands, but
    this isn't the correct way, because once the ovs port is created,
    neutron ove agent will monitor the chagne immediately and deals
    with the new added port, however, at this time, the other params
    are not set in ovs, this patch is to make creating ovs port and
    setting corresponding params in one operation
    In this patch, we add new Dom0 plugin functions ovs_create_port()
    and wrapper file command.py for calling Dom0 plugin
    
    Change-Id: Ic7bab9fd3eeed8be3f1493716178189bcf048da1
    Partial-Bug: #1649747

--- os_xenapi/dom0/etc/xapi.d/plugins/xenhost.py
+++ os_xenapi/dom0/etc/xapi.d/plugins/xenhost.py
@@ -260,6 +260,21 @@ def _ovs_add_port(args):
     return _run_command(cmd_args)
 
 
+def _ovs_create_port(args):
+    bridge = pluginlib.exists(args, 'bridge')
+    port = pluginlib.exists(args, 'port')
+    iface_id = pluginlib.exists(args, 'iface-id')
+    mac = pluginlib.exists(args, 'mac')
+    status = pluginlib.exists(args, 'status')
+    cmd_args = ['ovs-vsctl', '--', '--if-exists', 'del-port', port,
+                '--', 'add-port', bridge, port,
+                '--', 'set', 'Interface', port,
+                'external_ids:iface-id=%s' % iface_id,
+                'external_ids:iface-status=%s' % status,
+                'external_ids:attached-mac=%s' % mac]
+    return _run_command(cmd_args)
+
+
 def _ip_link_get_dev(args):
     device_name = pluginlib.exists(args, 'device_name')
     cmd_args = ['ip', 'link', 'show', device_name]
@@ -338,6 +353,7 @@ ALLOWED_NETWORK_CMDS = {
     # allowed cmds to config OVS bridge
     'ovs_add_patch_port': _ovs_add_patch_port,
     'ovs_add_port': _ovs_add_port,
+    'ovs_create_port': _ovs_create_port,
     'ovs_del_port': _ovs_del_port,
     'ovs_del_br': _ovs_del_br,
     'ovs_set_if_external_id': _ovs_set_if_external_id,
