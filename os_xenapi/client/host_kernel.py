292fc47158e657659cb8b62e298a86c562b595b0
Huan Xie <huan.xie@citrix.com>
2017-04-04 23:36:03 -0700

    os-xenapi v2: Expose python interfaces for some Dom0 plugins
    This patch is to implement os-xenapi V2 which will provide python
    interface for calling dom0 plugins
    1. wrapper of xenhost.py for ovs and linux bridge part
    2. wrapper of partition_utils.py
    3. wrapper of kernel.py
    4. wrapper of migration.py
    5. wrapper of bandwidth.py in host_network.py
    6. wrapper of console.py
    7. wrapper of workarounds.py in host_kernel.py
    8. wrapper of ipxe.py
    9. wrapper of xenhost.py for host/power management
    
    Change-Id: Ib588577cd22d0d3b8cda634b82e7993571bec7e8

---
+++ os_xenapi/client/host_kernel.py
@@ -0,0 +1,44 @@
+# Copyright 2017 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+def copy_vdi(session, vdi_ref, vdi_size, image_id=None):
+    args = {}
+    args['vdi-ref'] = vdi_ref
+    args['image-size'] = str(vdi_size)
+    if image_id:
+        args['cached-image'] = image_id
+    session.call_plugin('kernel.py', 'copy_vdi', args)
+
+
+def create_kernel_ramdisk(session, image_id, new_image_uuid):
+    args = {}
+    args['cached-image'] = image_id
+    args['new-image-uuid'] = new_image_uuid
+    session.call_plugin('kernel.py', 'create_kernel_ramdisk', args)
+
+
+def remove_kernel_ramdisk(session, kernel_file=None, ramdisk_file=None):
+    args = {}
+    if kernel_file:
+        args['kernel-file'] = kernel_file
+    if ramdisk_file:
+        args['ramdisk-file'] = ramdisk_file
+    if args:
+        session.call_plugin('kernel.py', 'remove_kernel_ramdisk', args)
+
+
+def safe_copy_vdis(session, sr_path, vdi_uuids, uuid_stack):
+    return session.call_plugin_serialized(
+        'workarounds.py', 'safe_copy_vdis', sr_path, vdi_uuids, uuid_stack)
