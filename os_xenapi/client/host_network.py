309b247642cfac9ef40834e6e523f07b4c704fac
Huan Xie <huan.xie@citrix.com>
2017-04-04 23:36:03 -0700

    os-xenapi v2: Expose python interfaces for some Dom0 plugins
    This patch is to implement os-xenapi V2 which will provide python
    interface for calling dom0 plugins
    1. wrapper of xenhost.py for ovs and linux bridge part
    2. wrapper of partition_utils.py
    3. wrapper of kernel.py
    4. wrapper of migration.py
    5. wrapper of bandwidth.py in host_network.py
    6. wrapper of console.py
    7. wrapper of workarounds.py in host_kernel.py
    8. wrapper of ipxe.py
    
    Note: https://review.openstack.org/#/c/453493/ this is the nova
          patch that verify the correctness of this patch
    
    Change-Id: Ib588577cd22d0d3b8cda634b82e7993571bec7e8

--- os_xenapi/client/host_network.py
+++ os_xenapi/client/host_network.py
@@ -1,4 +1,4 @@
-# Copyright 2013 OpenStack Foundation
+# Copyright 2017 OpenStack Foundation
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -22,3 +22,113 @@ def ovs_create_port(session, bridge, port, iface_id, mac, status):
                      'status': status}
             }
     session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ovs_add_port(session, bridge, port):
+    args = {'cmd': 'ovs_add_port',
+            'args': {'bridge_name': bridge, 'port_name': port}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ovs_del_port(session, bridge, port):
+    args = {'cmd': 'ovs_del_port',
+            'args': {'bridge_name': bridge, 'port_name': port}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ovs_del_br(session, bridge_name):
+    args = {'cmd': 'ovs_del_br',
+            'args': {'bridge_name': bridge_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_add_if(session, bridge_name, interface_name):
+    args = {'cmd': 'brctl_add_if',
+            'args': {'bridge_name': bridge_name,
+                     'interface_name': interface_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_del_if(session, bridge_name, interface_name):
+    args = {'cmd': 'brctl_del_if',
+            'args': {'bridge_name': bridge_name,
+                     'interface_name': interface_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_del_br(session, bridge_name):
+    args = {'cmd': 'brctl_del_br',
+            'args': {'bridge_name': bridge_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_add_br(session, bridge_name):
+    args = {'cmd': 'brctl_add_br',
+            'args': {'bridge_name': bridge_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_set_fd(session, bridge_name, fd):
+    args = {'cmd': 'brctl_set_fd',
+            'args': {'bridge_name': bridge_name,
+                     'fd': fd}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def brctl_set_stp(session, bridge_name, stp_opt):
+    args = {'cmd': 'brctl_set_stp',
+            'args': {'bridge_name': bridge_name,
+                     'option': stp_opt}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ip_link_add_veth_pair(session, dev1_name, dev2_name):
+    args = {'cmd': 'ip_link_add_veth_pair',
+            'args': {'dev1_name': dev1_name,
+                     'dev2_name': dev2_name}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ip_link_del_dev(session, device):
+    args = {'cmd': 'ip_link_del_dev',
+            'args': {'device_name': device}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ip_link_get_dev(session, device):
+    args = {'cmd': 'ip_link_get_dev',
+            'args': {'device_name': device}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ip_link_set_dev(session, device, option):
+    args = {'cmd': 'ip_link_set_dev',
+            'args': {'device_name': device,
+                     'option': option}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def ip_link_set_promisc(session, device, promisc_option):
+    args = {'cmd': 'ip_link_set_promisc',
+            'args': {'device_name': device,
+                     'option': promisc_option}
+            }
+    session.call_plugin_serialized('xenhost.py', 'network_config', args)
+
+
+def fetch_all_bandwidth(session):
+    return session.call_plugin_serialized('bandwidth.py',
+                                          'fetch_all_bandwidth')
