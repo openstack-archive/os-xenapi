1974a253049c5a3a1c4af0eda94cb8442551d6f6
Huan Xie <huan.xie@citrix.com>
2017-04-04 23:36:03 -0700

    WIP: os-xenapi V2
    This patch is to implement os-xenapi V2 which will provide python
    interface for calling dom0 plugins
    1. wrapper of xenhost.py for ovs and linux bridge part
    2. wrapper of partition_utils.py
    3. wrapper of kernel.py
    4. wrapper of migration.py
    5. wrapper of bandwidth.py in client.host_network
    
    Change-Id: Ib588577cd22d0d3b8cda634b82e7993571bec7e8

---
+++ os_xenapi/client/host_migration.py
@@ -0,0 +1,24 @@
+# Copyright 2013 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+def transfer_vhd(session, instance_uuid, host, vdi_uuid, sr_path, seq_num):
+    session.call_plugin_serialized('migration.py', 'transfer_vhd',
+                                   instance_uuid, host, vdi_uuid, sr_path,
+                                   seq_num)
+
+
+def move_vhds_into_sr(session, instance_uuid, sr_path, uuid_stack):
+    return session.call_plugin_serialized('migration.py', 'move_vhds_into_sr',
+                                          instance_uuid, sr_path, uuid_stack)
