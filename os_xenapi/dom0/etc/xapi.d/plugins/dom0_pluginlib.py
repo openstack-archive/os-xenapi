67abc73ee82db3bc8de8326e124f6e62c9c98a1e
naichuans <naichuan.sun@citrix.com>
2017-04-07 01:24:53 +0000

    os-xenapi: add a maximum retry count for vbd unplug
    If the vbd unplug always return DEVICE_DETACH_REJECTED,
    it will be in loop for ever. Add a retry count to avoid endless loop.
    
    Change-Id: I1f3aaa2e9f692f14aec8115d476220a728d6a302
    Closes-Bug: #1592362

--- os_xenapi/dom0/etc/xapi.d/plugins/dom0_pluginlib.py
+++ os_xenapi/dom0/etc/xapi.d/plugins/dom0_pluginlib.py
@@ -113,13 +113,18 @@ def with_vdi_in_dom0(session, vdi, read_only, f):
         logging.debug('Destroying VBD for VDI %s done.', vdi)
 
 
-def _vbd_unplug_with_retry(session, vbd):
+DEFAULT_COUNT = 30
+
+
+def _vbd_unplug_with_retry(session, vbd, retrycount=DEFAULT_COUNT):
     """Call VBD.unplug on the given VBD
 
     with a retry if we get DEVICE_DETACH_REJECTED. For reasons which I don't
     understand, we're seeing the device still in use, even when all processes
     using the device should be dead.
+    add DEFAULT_COUNT for unit test reference
     """
+    rcount = retrycount
     while True:
         try:
             session.xenapi.VBD.unplug(vbd)
@@ -128,6 +133,10 @@ def _vbd_unplug_with_retry(session, vbd):
         except XenAPI.Failure, e:   # noqa
             if (len(e.details) > 0 and
                     e.details[0] == 'DEVICE_DETACH_REJECTED'):
+                rcount -= 1
+                if(rcount <= 0):
+                    logging.error('VBD.unplug failed after retry 30 times.')
+                    return
                 logging.debug('VBD.unplug rejected: retrying...')
                 time.sleep(1)
             elif (len(e.details) > 0 and
