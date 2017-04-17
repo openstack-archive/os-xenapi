63b69cf97b273dbe75f5b91af23e18454cbdb659
naichuans <naichuan.sun@citrix.com>
2017-04-17 03:38:22 +0000

    os-xenapi: fix TypeError in agent.py when throws an exception
    There are Grammatical errors in the file agent.py in os-xenapi. Check https://github.com/openstack/os-xenapi/blob/master/os_xenapi/dom0/etc/xapi.d/plugins/agent.py line 260/263.
    
    Change-Id: I9553ae51ed0e5714394cc5117dbe8fa86cc1583b
    Closes-Bug: #1682716

--- os_xenapi/dom0/etc/xapi.d/plugins/agent.py
+++ os_xenapi/dom0/etc/xapi.d/plugins/agent.py
@@ -43,6 +43,8 @@ import dom0_pluginlib
 dom0_pluginlib.configure_logging("agent")
 import xenstore
 
+from exceptions import NotImplementedError
+
 
 DEFAULT_TIMEOUT = 30
 PluginError = dom0_pluginlib.PluginError
@@ -257,10 +259,10 @@ def _wait_for_agent(self, request_id, arg_dict, timeout):
 
     if reboot_detected:
         raise RebootDetectedError("REBOOT: dom_id %s no longer "
-                                  "present") % arg_dict["dom_id"]
+                                  "present" % arg_dict["dom_id"])
     else:
         raise TimeoutError("TIMEOUT: No response from agent within"
-                           " %s seconds.") % timeout
+                           " %s seconds." % timeout)
 
 
 if __name__ == "__main__":
