8f2914e57e8e4b0a2ef841b1906f9a25c0c5d8a2
naichuans <naichuan.sun@citrix.com>
2017-04-25 02:04:47 +0000

    os-xenapi: add unit tests for agent.py
    Currently all our OpenStack Dom0 plugins are moved to os-xenapi,
    but some of them don't have any unit test, this task is to add unit
    test for *agent.py*.
    Add dir checking to avoid logging dir doesn't exist issue
    
    Change-Id: I4e96f21fd50b927e9bfd872b42feca6974ac80ae

--- os_xenapi/tests/plugins/test_dom0_pluginlib.py
+++ os_xenapi/tests/plugins/test_dom0_pluginlib.py
@@ -14,6 +14,7 @@
 #    under the License.
 
 import mock
+import os
 from os_xenapi.tests.plugins import plugin_test
 import time
 
@@ -28,7 +29,38 @@ class PluginlibDom0(plugin_test.PluginTestBase):
         super(PluginlibDom0, self).setUp()
         self.dom0_pluginlib = self.load_plugin("dom0_pluginlib.py")
 
-    def test_configure_logging(self):
+    def test_configure_logging_log_dir_not_exist(self):
+        self.mock_patch_object(os.path,
+                               'exists',
+                               False)
+        name = 'fake_name'
+        mock_Logger_setLevel = self.mock_patch_object(
+            self.dom0_pluginlib.logging.Logger, 'setLevel')
+        mock_sysh_setLevel = self.mock_patch_object(
+            self.dom0_pluginlib.logging.handlers.SysLogHandler, 'setLevel')
+        mock_Formatter = self.mock_patch_object(
+            self.dom0_pluginlib.logging, 'Formatter')
+        mock_sysh_setFormatter = self.mock_patch_object(
+            self.dom0_pluginlib.logging.handlers.SysLogHandler, 'setFormatter')
+        mock_Logger_addHandler = self.mock_patch_object(
+            self.dom0_pluginlib.logging.Logger, 'addHandler')
+        mock_socket = self.mock_patch_object(
+            self.dom0_pluginlib.logging.handlers.SysLogHandler,
+            '_connect_unixsocket')
+
+        self.dom0_pluginlib.configure_logging(name)
+
+        self.assertFalse(mock_Logger_setLevel.called)
+        self.assertFalse(mock_sysh_setLevel.called)
+        self.assertFalse(mock_Formatter.called)
+        self.assertFalse(mock_sysh_setFormatter.called)
+        self.assertFalse(mock_Logger_addHandler.called)
+        self.assertFalse(mock_socket.called)
+
+    def test_configure_logging_log(self):
+        self.mock_patch_object(os.path,
+                               'exists',
+                               True)
         name = 'fake_name'
         mock_Logger_setLevel = self.mock_patch_object(
             self.dom0_pluginlib.logging.Logger, 'setLevel')
