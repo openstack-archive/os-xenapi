08aa875979fcd0d7bce584485556782cce3e71a8
Jianghua Wang <jianghua.wang@citrix.com>
2017-12-12 09:29:11 +0000

    Introduce some utils for kolla deployment.
    This commit introduces commands which can be invoked by kolla
    playbook to:
    1). bootstrap XenAPI - xenapi_bootstrap;
    2). get XenAPI specific facts which can be used by kolla to
    generate OpenStack configure files. - xenapi_get_facts
    
    Note: This commit only includes interface definitions; the following
    patches will implement the real functions.
    
    Change-Id: Ie2d7d40f2755580aac4a10f3d302190a8bd4fe6f

---
+++ os_xenapi/utils/get_xenapi_facts.py
@@ -0,0 +1,35 @@
+# Copyright 2017 Citrix Systems
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+
+
+def _get_dom0_hostname():
+    # TODO: get dom0's hostname
+    HOSTNAME = "traya"
+    return HOSTNAME
+
+def _get_himn_local_ip():
+    # TODO: detect local HIMN interface and return the allocated IP address.
+    IP = '169.254.0.2'
+    return IP
+
+def main():
+    facts = {}
+    facts['DOM0_HOSTNAME'] = _get_dom0_hostname()
+    facts['HIMN_LOCAL_IP'] = _get_himn_local_ip()
+    print(json.dumps(facts))
+
+if __name__ == "__main__":
+    sys.exit(main())
