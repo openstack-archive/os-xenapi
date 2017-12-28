c12f405162457bb84665b7462dcad463c3ce15c2
naichuans <naichuan.sun@citrix.com>
2017-12-28 08:23:23 +0000

    os-xenapi: add unit test for himn utils
    Add unit test for himn utils
    
    Change-Id: I5154b5ed3346f8c5d81e117ad09351be4fd7e9ff

--- os_xenapi/client/exception.py
+++ os_xenapi/client/exception.py
@@ -77,3 +77,15 @@ class VdiImportFailure(OsXenApiException):
 
 class VhdDiskTypeNotSupported(OsXenApiException):
     msg_fmt = _("Not supported VHD disk type: type=(%(disk_type)s)")
+
+
+class NoNetworkInterfaceWithIp(OsXenApiException):
+    msg_fmt = _("Can't find network interface: ip=(%(ip)s) ")
+
+
+class ExecuteCommandFailed(OsXenApiException):
+    msg_fmt = _("Execute command failed: cmd=(%(cmd)s)")
+
+
+class NoNetworkInterfaceWithMac(OsXenApiException):
+    msg_fmt = _("Cannot find eth matches mac: mac=(%(mac)s)")
