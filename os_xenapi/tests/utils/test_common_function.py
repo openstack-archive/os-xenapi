acb063fac0de73809bb42e08e083301b15fca1c1
Jianghua Wang <jianghua.wang@citrix.com>
2018-01-09 06:20:12 +0000

    Kolla-on-XenServer: Add dom0's ip addresses into xenapi facts
    This commit is to add dom0's IPs in the xenapi facts. So that
    kolla-ansible can use the proper IP address for vxlan tunnel or
    other places when require dom0's IPs.
    
    Change-Id: Iede11b4a529ff1885b588bf01d7ff15dc6185092

--- os_xenapi/tests/utils/test_common_function.py
+++ os_xenapi/tests/utils/test_common_function.py
@@ -27,3 +27,32 @@ class CommonUtilFuncTestCase(base.TestCase):
 
         self.assertEqual(hostname, 'Fake_host_name')
         mock_client.ssh.assert_called_with('hostname')
+
+    def test_get_host_ipv4s(self):
+        mock_client = mock.Mock()
+        out = u'xenbr0 10.71.64.118/20\n'
+        out += 'xenapi 169.254.0.1/16\n'
+        err = ''
+        mock_client.ssh.return_value = (out, err)
+
+        ipv4s = common_function.get_host_ipv4s(mock_client)
+
+        expect = [
+            {
+                "address": "10.71.64.118",
+                "broadcast": "10.71.79.255",
+                "interface": "xenbr0",
+                "netmask": "255.255.240.0",
+                "network": "10.71.64.0"
+            },
+            {
+                "address": "169.254.0.1",
+                "broadcast": "169.254.255.255",
+                "interface": "xenapi",
+                "netmask": "255.255.0.0",
+                "network": "169.254.0.0"
+            }
+        ]
+
+        self.assertEqual(ipv4s, expect)
+        mock_client.ssh.assert_called()
--- os_xenapi/tests/utils/test_xenapi_facts.py
+++ os_xenapi/tests/utils/test_xenapi_facts.py
@@ -20,21 +20,40 @@ from os_xenapi.utils import xenapi_facts
 
 
 class XenapiFactsTestCase(base.TestCase):
+    @mock.patch.object(common_function, 'get_host_ipv4s')
     @mock.patch.object(common_function, 'get_remote_hostname')
     @mock.patch.object(himn, 'get_local_himn_eth')
     @mock.patch.object(netifaces, 'ifaddresses')
-    def test_get_facts(self, mock_ifaddr, mock_eth, mock_hostname):
+    def test_get_facts(self, mock_ifaddr, mock_eth, mock_hostname, mock_ip):
         mock_client = mock.Mock()
         mock_client.ip = mock.sentinel.dom0_himn_ip
         mock_eth.return_value = 'eth3'
         mock_ifaddr.return_value = {2: [{'netmask': u'255.255.0.0',
                                          'addr': u'169.254.0.2'}]}
         mock_hostname.return_value = 'traya'
+        fake_ipv4s = [
+            {
+                "address": "10.71.64.118",
+                "broadcast": "10.71.79.255",
+                "interface": "xenbr0",
+                "netmask": "255.255.240.0",
+                "network": "10.71.64.0"
+            },
+            {
+                "address": "169.254.0.1",
+                "broadcast": "169.254.255.255",
+                "interface": "xenapi",
+                "netmask": "255.255.0.0",
+                "network": "169.254.0.0"
+            }
+        ]
+        mock_ip.return_value = fake_ipv4s
 
         ret_facts = xenapi_facts.get_xenapi_facts(mock_client)
 
         expect_facts = {"domu_himn_ip": "169.254.0.2",
                         "domu_himn_eth": "eth3",
-                        "dom0_hostname": "traya"}
+                        "dom0_hostname": "traya",
+                        "dom0_ipv4s": fake_ipv4s}
         self.assertEqual(ret_facts, expect_facts)
         mock_eth.assert_called_with(mock.sentinel.dom0_himn_ip)
