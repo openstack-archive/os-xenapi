3cd6e5bce46f1aa224d313da62794d9138d0dd81
Jianghua Wang <jianghua.wang@citrix.com>
2018-01-19 08:57:52 +0000

    XenAPI-Kolla: Gather domU's vifs in facts
    This commit is to gather domU's vifs in facts. So that the compute
    VM's interfaces' vif information will be included in the xenapi_facts.
    When deploy OpenStack on XenServers, the vif information can be used
    by deployment scripts (e.g. in Kolla-ansible, it can use the vif's
    bridge to support provider networks by setting the bridge mappings).
    
    Change-Id: I9a6bebe19ed488bb2173d5dc2daa14e236411243

--- os_xenapi/tests/utils/test_common_function.py
+++ os_xenapi/tests/utils/test_common_function.py
@@ -56,3 +56,64 @@ class CommonUtilFuncTestCase(base.TestCase):
 
         self.assertEqual(ipv4s, expect)
         mock_client.ssh.assert_called()
+
+    def test_get_vm_vifs(self):
+        mock_client = mock.Mock()
+        vm_uuid = '9eeeea9f-de18-f101-fcc2-ae7366b540f2'
+        vif_list_data = u'0\r\n\n1\r\n'
+
+        vif_0_data = u'vif-id = "0"\r\n\n'
+        vif_0_data += u'mac = "9a:77:18:20:cf:14"\r\n\n'
+        vif_0_data += u'bridge = "xapi1"\r\n'
+
+        vif_1_data = u'vif-id = "1"\r\n\n'
+        vif_1_data += u'mac = "02:e3:69:a6:7b:b8"\r\n\n'
+        vif_1_data += u'bridge = "xapi0"\r\n'
+
+        mock_client.ssh.side_effect = [(vif_list_data, ''),  # xenstore-list
+                                       (vif_0_data, ''),  # xenstore-ls - vif 0
+                                       (vif_1_data, ''),  # xenstore-ls - vif 1
+                                       ]
+
+        expect = [{u'bridge': u'xapi1',
+                   u'mac': u'9a:77:18:20:cf:14',
+                   u'vif-id': u'0'},
+                  {u'bridge': u'xapi0',
+                   u'mac': u'02:e3:69:a6:7b:b8',
+                   u'vif-id': u'1'}]
+        vifs = common_function.get_vm_vifs(mock_client, vm_uuid)
+        self.assertEqual(vifs, expect)
+
+    @mock.patch.object(common_function.netifaces, 'ifaddresses')
+    @mock.patch.object(common_function.netifaces, 'interfaces')
+    @mock.patch.object(common_function, 'execute')
+    @mock.patch.object(common_function, 'get_vm_vifs')
+    def test_get_domu_vifs_by_eth(self, mock_get, mock_exec,
+                                  mock_if, mock_ifaddr):
+        mock_client = mock.Mock()
+        vm_uuid = '9eeeea9f-de18-f101-fcc2-ae7366b540f2'
+        mock_exec.return_value = '/vm/%s' % vm_uuid
+        vif_0 = {u'vif-id': u'0',
+                 u'bridge': u'xapi1',
+                 u'mac': u'9a:77:18:20:cf:14'}
+        vif_1 = {u'vif-id': u'1',
+                 u'bridge': u'xapi0',
+                 u'mac': u'02:e3:69:a6:7b:b8'}
+
+        mock_get.return_value = [vif_0, vif_1]
+        mock_if.return_value = ['eth0', 'eth1']
+        AF_LINK = common_function.netifaces.AF_LINK
+        mock_ifaddr.side_effect = [
+            {AF_LINK: [{u'addr': u'9a:77:18:20:cf:14'}]},
+            {AF_LINK: [{u'addr': u'02:e3:69:a6:7b:b8'}]}]
+
+        vifs_by_eth = common_function.get_domu_vifs_by_eth(mock_client)
+
+        expect = {'eth0': vif_0,
+                  'eth1': vif_1}
+        self.assertEqual(vifs_by_eth, expect)
+        mock_exec.assert_called_with('xenstore-read', 'vm')
+        mock_get.assert_called_with(mock_client, vm_uuid)
+        mock_if.assert_called_with()
+        self.assertEqual(mock_ifaddr.call_args_list,
+                         [mock.call('eth0'), mock.call('eth1')])
